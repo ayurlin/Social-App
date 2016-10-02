d5b24c165f28a2ad4ea216d4dde968fe50b75018
Aleksandra Meka <aleksandra.meka@gmail.com>
2016-10-02 19:06:40 +0200

    Add default avatar & remove avatar option

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -74,7 +74,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar, :remove_avatar)
     end
 
     def check_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -35,4 +35,5 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+  
 end
