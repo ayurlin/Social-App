456bbfb04ea746b87eb981383fd316e6afbe2582
Aleksandra Meka <aleksandra.meka@gmail.com>
2016-10-05 11:14:51 +0200

    Upgrade views

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,10 @@
+// $( document ).ready(function() {
+    
+//   // Add preview of avatar in form
+//   var preview = $('#user_avatar').val();
+
+//   if ( (preview).length > 0 ) {
+//     $('.avatar-preview').append("<img src=uploads/"+preview+"/>");
+//   }
+
+// });
