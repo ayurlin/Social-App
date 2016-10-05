f8772ffc4b5e9904009f55e4ea0a48eb8654641e
Aleksandra Meka <aleksandra.meka@gmail.com>
2016-10-05 18:06:31 +0200

    Drop db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161005093329) do
+ActiveRecord::Schema.define(version: 20161003121004) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20161005093329) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
   create_table "posts", force: :cascade do |t|
     t.text     "content"
     t.integer  "user_id"
