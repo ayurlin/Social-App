d07c5c657a360460fccd27cc609e80e1cd162f8b
Aleksandra Meka <aleksandra.meka@gmail.com>
2016-10-06 09:32:43 +0200

    Change post edit redirect

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -40,9 +40,10 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
+    @user = User.find(params[:id])
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.html { redirect_to @user, notice: 'Post was successfully updated.' }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit }
