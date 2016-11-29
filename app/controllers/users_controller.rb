e910b69653b581641527e01660f75728b4ce6fe9
Aleksandra Meka <aleksandra.meka@gmail.com>
2016-11-29 16:33:27 +0100

    Disable current user from searched users list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     if params[:search]
-      @users = User.search(params[:search]).order("created_at DESC")
-    else
-      @users = User.all.order("created_at DESC")
+      @users = User.where.not(id: current_user.id).search(params[:search]).order("created_at DESC")
     end
   end
 
